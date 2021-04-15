cb8298b95319ed0595819b6702bc00e57814fbd4
VcelistMC <60217976+VcelistMC@users.noreply.github.com>
2021-04-15 11:25:02 +0200

    added get endpoints

--- server.js
+++ server.js
@@ -2,18 +2,41 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const PORT = 8080;
+app.listen(
+    PORT,
+    function () {
+        console.log(`Server running on ${PORT}`);
+    }
+)
+
+app.get('/all', function (req, res) {
+    res.send(projectData);
+})
+
+app.post('/add', function (req, res) {
+    const data = req.body;
+    var newObj = {
+        temp: data.temperature,
+        date: data.date,
+        user_res: data.feelings
+    }
+    projectData.push(newObj);
+    console.log(newObj);
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,31 @@
 /* Global Variables */
-
+const KEY = '&appid=e10c89103e701eaf19221327ec9ba201';
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const DEF_CODE = '07463';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+let genButton = document.getElementById('generate');
+genButton.addEventListener('click', clickAction);
+
+function clickAction(event){
+    console.log("boop!");
+    let textBox = document.getElementById('zip');
+    let feelingsBox = document.getElementById('feelings');
+    var userZIP = textBox.value;
+    var userFeelings = feelingsBox.value;
+}
+
+let getData = async(url = '', key = '', code = '') => {
+    const req = await fetch(url + code + key);
+    try {
+        const res = await req.json();
+        console.log(res); 
+    } catch (error) {
+        console.log(`error: ${error}`);
+    }
+}
+
+
+//getData(BASE_URL + DEF_CODE + KEY);
